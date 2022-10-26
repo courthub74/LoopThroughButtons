edebb3f35d13e083eee21a48fba0a14f2ac83624
Courdevelops <courdevelops@gmail.com>
2022-10-26 16:38:57 -0400

    Event Delegation used

--- buttons.js
+++ buttons.js
@@ -1,13 +1,33 @@
 // lets loop through the buttons to the console
 
+// try using EVENT DELEGATION
+
+// first get the buttons by PARENT id and event listener
+document.getElementById("buttons").addEventListener('click', function(e){
+    // if statement by event target for the event function
+        // the event target is the elements by id within the parent div
+    if (e.target && e.target.id == "button"){
+        console.log("Ready for the functionality");
+        // test print the innerText of the button
+        console.log(buttons.innerText);
+        // set the innerText of each button pressed to something different
+        let button1 = document.getElementsByClassName("button1");
+        console.log(button1);
+        if (button1.innerText == "Button 1"){
+            button1.innerText = "Click'd";
+            console.log("test");
+        }
+    }
+});
+
 // first get the buttons by ID
-let buttons = document.getElementsByTagName("button");
+// let buttons = document.getElementsByTagName("button");
 
 // test print the buttons
 // console.log(buttons);
 
 // get the ID of button 1
-let button1 = document.getElementById("button1");
+// let button1 = document.getElementById("button");
 
 // test print the button 1
 // console.log(button1);
@@ -33,39 +53,39 @@ let button1 = document.getElementById("button1");
 // button1.addEventListener('click', button1func);
 
 // NOW we must learn how to loop through these
-for (var i = 0; i < buttons.length; i++){
-    // test print the buttons
-    console.log(buttons[i]);
-    const buttonsel = buttons[i].innerHTML;
-    console.log(buttonsel);
-}
+// for (var i = 0; i < buttons.length; i++){
+//     // test print the buttons
+//     console.log(buttons[i]);
+//     const buttonsel = buttons[i].innerHTML;
+//     console.log(buttonsel);
+// }
 
 // maybe do a function that loops through all the buttons and then does the if else shit
-function loopbuttons(){
-    for (var i = 0; i < buttons.length; i++){
-        // test print the buttons
-        // console.log(buttons[i]);
-        let allbuttons = buttons[i];
-        // change the text to clicked on each button
-        if (allbuttons.innerText == "Button") {
-            allbuttons.innerText = "Click'd";
-            console.log("Button Clicked");
-        } else {
+// function loopbuttons(){
+//     for (var i = 0; i < buttons.length; i++){
+//         // test print the buttons[]
+//         // console.log(buttons[i]);
+//         let allbuttons = buttons[i];
+//         // change the text to clicked on each button
+//         if (allbuttons.innerText == "Button") {
+//             allbuttons.innerText = "Click'd";
+//             console.log("Button Clicked");
+//         } else {
 
-        } if (allbuttons.innerText == "Click'd"){
-            allbuttons.innerText = "Button";
-            console.log("Button Clicked Again");
-        }
-        console.log(allbuttons);
-        // add an event listener and call the function to all the buttons
-        allbuttons.addEventListener('click', loopbuttons);
-    }
-};
+//         } if (allbuttons.innerText == "Click'd"){
+//             allbuttons.innerText = "Button";
+//             console.log("Button Clicked Again");
+//         }
+//         console.log(allbuttons);
+//         // add an event listener and call the function to all the buttons
+//         // allbuttons.addEventListener('click', loopbuttons);
+//     }
+// };
 
-buttons.addEventListener('click', loopbuttons);
+// buttons.addEventListener('click', loopbuttons);
 
 
-// prototype on how to create and loop through buttons
+// prototype on how to create and loop through buttons by creating them in the DOM
 
     // querySelect the list 
     // const (theLIorDiv) = document.querySelector('.thelist');
