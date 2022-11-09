8498900231e0e13cac4f0792b9714eb610984890
Courdevelops <courdevelops@gmail.com>
2022-11-09 12:36:03 -0500

    action buttons started

--- crossbuttonform/crossbuttonform.js
+++ crossbuttonform/crossbuttonform.js
@@ -37,12 +37,20 @@ window.addEventListener('load', () => {
             return;
         }
 
+
+        // TODOS DIV
         // create the "todos" div
         const todo_div = document.createElement('div');
 
         // add the class to the div
         todo_div.classList.add('todos');
 
+        // append to the todos-list div
+        todo_list_element.appendChild(todo_div);
+
+
+        
+        // CONTENT DIV
         // create "content" div
         const todo_content_div = document.createElement('div');
         
@@ -52,10 +60,59 @@ window.addEventListener('load', () => {
         // append the "todos" div to the "content" div
         todo_div.appendChild(todo_content_div);
 
+
+
+        // INPUT ELEMENT
         // create the input element that's going to be inside of the content
             // which is actually a display of the output
                 // but you need to set as readonly so ....
-        
+        const todo_input_element = document.createElement('input');
+        // set it's class
+        todo_input_element.classList.add('text');
+        // set it's type
+        todo_input_element.type = "text";
+        // set it's value. Which is the input value 'todo' variable
+        todo_input_element.value = todo;
+        // set it's attribute
+        todo_input_element.setAttribute('readonly', true);
 
-    })
+        // append the input element to the content div
+            // which was appended to the todos div earlier
+        todo_content_div.appendChild(todo_input_element);
+
+
+
+        // ACTION ELEMENT
+        // this is the bar of buttons (edit, delete, cross-off)
+        todo_actions_div = document.createElement('div');
+        // set it's class
+        todo_actions_div.classList.add('actions');
+
+
+        // EDIT BUTTON
+        // create the edit button
+        const todo_edit_button = document.createElement('button');
+        // set it's class
+        todo_edit_button.classList.add('edit');
+        // set the inner text
+        todo_edit_button.innerText = "Edit";
+
+        // DELETE BUTTON
+        // create the delete button
+        const todo_delete_button = document.createElement('button');
+        // set it's class
+        todo_delete_button.classList.add('delete');
+        // set the inner text
+        todo_delete_button.innerText = "Delete";
+
+        // CROSS BUTTON
+        // create the cross button
+        const todo_cross_button = document.createElement('button');
+        // set it's class
+        todo_cross_button.classList.add('cross');
+        // set the inner text
+        todo_cross_button.innerText = "Cross-Off";
+
+
+    });
 });
