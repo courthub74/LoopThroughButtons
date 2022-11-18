d0c15df767cfc093fd6b1f8e66ef79f5a6cbcfa6
Courdevelops <courdevelops@gmail.com>
2022-11-18 16:26:54 -0500

    edit button added

--- crossbuttonformfirebase/LoopThroughButtons-laptop-master/LoopThroughButtons-laptop-master/crossbuttonformfirebase/editbuttonfb.js
+++ crossbuttonformfirebase/LoopThroughButtons-laptop-master/LoopThroughButtons-laptop-master/crossbuttonformfirebase/editbuttonfb.js
@@ -128,7 +128,88 @@ window.addEventListener('load', () => {
         // test print it
         console.log(todo_div);
 
-        
+        // add the class to the div
+        todo_div.classList.add('todos');
+
+        // append the todo div to the todos-list
+        todo_list_element.appendChild(todo_div);
+
+        // CONTENT DIV
+
+        // create the 'content' div 
+                // this is setup as an input that displays the output
+        const todo_content_div = document.createElement('div');
+
+        // test print it
+        console.log(todo_content_div);
+
+        // add the class to the div
+        todo_content_div.classList.add('content');
+
+        // append the content div to the todo div
+        todo_div.appendChild(todo_content_div);
+
+        // INPUT ELEMENT
+            // create the input element that's going to be inside of the content
+            // which is actually a display of the output
+                // but you need to set as readonly so ....
+        const todo_input_element = document.createElement('input');
+
+        // test print it
+        console.log(todo_input_element);
+
+        // add it's class
+        todo_input_element.classList.add('input');
+        // set it's type
+        todo_input_element.type = "text";
+         // set it's id
+         todo_input_element.id = "input";
+         // ABOVE you may need to assign an iterative id 
+             // to correspond to each button
+
+        // set it's value. Which is the input value 'todo' variable
+                // you need to redefine 'todo' in this for loop
+        todo_input_element.value = todosall.item;
+        // set it's style I just wanted it wider may just make a css sheet
+        todo_input_element.style.width = "300px";
+        // set it's attribute
+        todo_input_element.setAttribute('readonly', true);
+
+        // append the input element to the content div
+            // which was appended to the todos div earlier
+        todo_content_div.appendChild(todo_input_element);
+
+
+        // ACTION ELEMENT
+
+        // this is the bar of buttons (edit, delete, cross-off)
+        const todo_actions_div = document.createElement('div');
+         // test print
+         console.log(todo_actions_div);
+         // set it's class
+         todo_actions_div.classList.add('actions');
+
+         // append the actions div to the todos div
+             // REMEMBER parent first and then appendChild to it
+                 // this is separate from the input on the todos div
+         todo_div.appendChild(todo_actions_div);
+
+        // EDIT BUTTON
+        // create the edit button
+        const todo_edit_button = document.createElement('button');
+        // test print
+        console.log(todo_edit_button);
+        // set it's class
+        todo_edit_button.classList.add('edit');
+        // set it's id
+        todo_edit_button.setAttribute('id', todosall.id);
+        // set the innerText
+        todo_edit_button.innerText = "EDIT";
+        // test print
+        console.log(todo_edit_button);
+
+        // append edit button to the actions div
+        todo_actions_div.appendChild(todo_edit_button);
     }
 
     }
